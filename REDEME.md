d6762a80f5227282ea1a9401fbdda177c9086bcf
qovciu <372903575@qq.com>
2015-03-04 04:14:41 +0800

    some bug

---
+++ REDEME.md
@@ -0,0 +1,14 @@
+##Socket.Io+
+
+I wanna say  something for Socket.Io+ . i think the Socket.Io is so powerful.. But something error. When i use the Socket developer my project. I had to repeat it some code again. I think, we need a puls of Socket.Io. Let us developer some general project easier than before.
+
+So . What is **puls** ? **+** ?
+
+### what is we used to do ? ###
+
+1. when i first connection the service. we need to provide some information. And get some information from the service.  : socket (information)
+2. join . send. exit. list . so. socket.js
+3. maybe , i will write my socket.io~
+
+:P
+
--- socket.js
+++ socket.js
@@ -1,36 +1,33 @@
 ﻿
-// *******************************************
-// * _ (暂定名字)
-// *******************************************
-// *
-// * var _ = new _( 你的名字 );
-// * _.join( 房间名, 回调函数 );
-// * 
-// * 回调函数会传回一个msg对象 --> { from: "" , data :"" } msg.from 来源 , msg.data 数据
-// *
-// * _.send( 数据, 房间名 );
-// *
-// *
-// ******************************************* 
 function socket(user, url) {
     this.user = user;
-    this.io = (url ? io(url) : io('http://localhost:3000/'));
+    this.me = this.io = (url ? io(url) : io('http://localhost:3000/'));
 }
 
 socket.prototype = {
+    listListen :[],
     join: function (house, fn) {//房间 用户
+        if(!house || !fn){ return false; }
         this.io.on(house, fn);
         this.io.emit("join", { type: "join", house: house, from: this.user });
 
     },
     send: function (data, house) {
+        if(!data || !house){ return false; }
         this.io.emit("send", { type: "send", house: house, from: this.user, data: data });
     },
     exit: function (house) {
+        if(!house){ return false; }
         this.io.emit("exit", { type: "exit", house: house, from: this.user });
     },
     list: function (house, fn) {
-        this.io.emit("list", { type: "exit", house: house, from: this.user });
+        if(!house || !fn){ return false; }
+        if (!~this.listListen.indexOf(house)) {
+            this.listListen.push(house);
+        } else {
+            this.io.off(socket.io.id);            
+        }
         this.io.on(socket.io.id, fn);
+        this.io.emit("list", {house: house, from: this.user });
     }
 }
